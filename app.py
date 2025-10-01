cfd37c1ddf14a59b7f93df7ac23dd9a9739acba4
Resta Gustina <152772347+Restagustina@users.noreply.github.com>
2025-10-01 11:29:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -71,14 +71,14 @@ def set_background(image_path):
             }}
             /* 1. Sidebar Transparan (Opacity 80%) */
             [data-testid="stSidebar"] {{
-                background-color: rgba(0, 0, 0, 0.8); 
+                background-color: rgba(255, 255, 255, 0.8); 
                 border-right: 1px solid #ccc;
             }}
 
             /* 2. AREA KONTEN UTAMA DIBUAT BURAM (Opacity 90%) */
             /* stVerticalBlock adalah container utama untuk konten dashboard */
             [data-testid="stVerticalBlock"] {{
-                background-color: rgba(255, 255, 255, 0.9);
+                background-color: rgba(0, 0, 0, 0.9);
                 padding: 10px 20px 20px 20px; 
                 border-radius: 10px; 
             }}
@@ -862,5 +862,6 @@ elif st.session_state.app_mode == 'admin_analytics' and st.session_state.user_ro
     st.markdown("---")
     st.subheader("Tabel Log Transaksi Terakhir")
     st.dataframe(df_log_filtered.tail(100).sort_values(by='timestamp', ascending=False), use_container_width=True)
+
 
 
