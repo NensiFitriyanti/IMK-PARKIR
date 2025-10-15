fee7c1201fbb3ebc389931530bc56499bf6e7966
Resta Gustina <152772347+Restagustina@users.noreply.github.com>
2025-10-15 15:11:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -722,6 +722,21 @@ if st.session_state.app_mode not in ['login', 'register']:
 # --------------------------------------------------------------
 # HALAMAN LOGIN
 # --------------------------------------------------------------
+# Tambahkan CSS biar tampilan login lebih kecil dan terpusat
+st.markdown("""
+    <style>
+    .stApp {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .main > div {
+        max-width: 350px; /* ubah ukuran di sini kalau mau lebih kecil */
+        margin: auto;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
 if st.session_state.app_mode == 'login':
     st.title("🅿️ Aplikasi Parkir Barcode")
     st.subheader("Selamat Datang! Silakan Login")
@@ -1173,6 +1188,7 @@ elif st.session_state.app_mode == 'admin_analytics' and st.session_state.user_ro
     st.markdown("---")
     st.subheader("Tabel Log Transaksi Terakhir")
     st.dataframe(df_log_filtered.tail(100).sort_values(by='timestamp', ascending=False), use_container_width=True)
+
 
 
 
